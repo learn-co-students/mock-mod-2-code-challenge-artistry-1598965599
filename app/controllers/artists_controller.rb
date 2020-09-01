7dcf7cc69b09c7b89c428c025f397983d7bdfb8c
Chenyun Zhang <morningchenyun@gmail.com>
2020-09-01 18:20:29 -0400

    done

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -9,20 +9,22 @@ class ArtistsController < ApplicationController
     end
 
     def new
+        @errors = flash[:errors]
         @artist = Artist.new
     end
     
     def create
         @artist = Artist.create(artist_param)
         
-        if artist_param[:id]
+        if @artist.valid?
             redirect_to artist_path(@artist)
         else
+            flash[:errors] = @artist.errors.full_messages
             redirect_to new_artist_path
         end
     end
 
-    private 
+    private
     def artist_param
         params.require(:artist).permit(:name,:title,:age)
     end

f42b1d523c9d1b5e3a709382cd0857fa40a3d85f
wilsonng21 <wilsongch92@gmail.com>
2020-09-01 10:34:51 -0400

    index pages work

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,13 @@
 Rails.application.routes.draw do
-  resources :artist_instruments
-  resources :instruments, only: [:index]
-  get "/artists", to: "artists#index", as: "artists"
+  get '/instruments', to: 'instruments#index', as: 'instruments'
+
+  get '/artists', to: 'artists#index', as: 'artists'
+  get '/artists/:id', to: 'artists#show', as: 'artist'
+  get '/artists/new', to: 'artists#new', as: 'new_artist'
+  post '/artists', to: 'artists#create'
+
+  get '/artist_instruments/new', to: 'artist_instruments#new', as: 'new_artist_instrument'
+
+  post '/artist_instruments', to: 'artist_instruments#create'
+
 end

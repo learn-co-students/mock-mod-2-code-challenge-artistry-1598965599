be248cd4900dc53957a51d0aed63182279ace477
attenbl19 <attenboroughl@protonmail.com>
2020-09-01 10:46:52 -0400

    adding show page to artists in vew

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,5 +3,18 @@ class ArtistsController < ApplicationController
     def index
         @artists = Artist.all
     end
+
+    def show
+        @artists = Artist.find(artists[:id])
+    end
+
+    def new
+        @artists = Artist.new
+    end
+
+    # def create({:Artist params[:]})
+
+    # end
+
     
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   resources :instruments, only: [:index]
   get "/artists", to: "artists#index", as: "artists"
+  get "/artists/:id", to: "artists#show", as: "artist"
 end
